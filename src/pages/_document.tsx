17de7e7987ecef1e165fda56dad98b4692461272
DESKTOP-A4OL20C\charl <charlotte@burns.net>
2025-02-19 02:47:11 +0000

    layout causing problems

---
+++ src/pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <title>Heather's Portfolio</title>
+        <meta name="description" content="Online Graphic Design Portfolio" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
