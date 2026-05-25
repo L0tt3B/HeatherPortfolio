253fcfbcd9ee3bc4ce9725472ddd2656b54aec35
DESKTOP-A4OL20C\charl <charlotte@burns.net>
2026-05-25 13:40:27 +0100

    fix bag links

--- src/app/bag.tsx
+++ src/app/bag.tsx
@@ -18,21 +18,21 @@ const Bag = ({ onTabChange, onScrollToFooter }: BagProps) => {
           className="max-w-full h-auto sm:max-w-[80%] md:max-w-[60%] cursor-pointer"
         />
       </div>
-      <div className="flex flex-wrap justify-center font-custom text-amber-400 text-xl mt-6 sm:text-lg sm:mt-4 space-x-6 sm:space-x-4 space-y-3 sm:space-y-2">
-        <a className="my-2 relative group cursor-pointer text-center inline-block"
+      <div className="flex flex-wrap justify-center font-custom text-amber-400 text-xl mt-6 sm:text-lg sm:mt-4 gap-6 sm:gap-8">
+        <a className="relative group cursor-pointer text-center inline-block"
             onClick={() => onTabChange("AboutMe")}> 
           About Me
-          <span className="block absolute left-1/2 w-0 h-[2px] bg-amber-400 group-hover:w-full transition-all duration-300 ease-in-out transform -translate-x-1/2"></span>
+          <span className="block absolute left-1/2 w-0 h-[2px] bg-amber-400 group-hover:w-full transition-all duration-300 ease-in-out transform -translate-x-1/2 bottom-[-2px]"></span>
         </a>
-        <a className="my-2 relative group cursor-pointer text-center inline-block"
+        <a className="relative group cursor-pointer text-center inline-block"
             onClick={() => { onScrollToFooter(); }}>
           Contact
           <span className="block absolute left-1/2 w-0 h-[2px] bg-amber-400 group-hover:w-full transition-all duration-300 ease-in-out transform -translate-x-1/2 bottom-[-2px]"></span>
         </a>
-        <a className="mx-4 relative group cursor-pointer text-center inline-block"
+        <a className="relative group cursor-pointer text-center inline-block"
             onClick={() => onTabChange("CV")}> 
           CV
-          <span className="block absolute left-1/2 w-0 h-[2px] bg-amber-400 group-hover:w-full transition-all duration-300 ease-in-out transform -translate-x-1/2"></span>
+          <span className="block absolute left-1/2 w-0 h-[2px] bg-amber-400 group-hover:w-full transition-all duration-300 ease-in-out transform -translate-x-1/2 bottom-[-2px]"></span>
         </a>
       </div>
     </div>
